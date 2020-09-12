edd23f307616dd0c0e1199278c028c8c4ac258ed
Erick Márquez <erick.marquez@britecore.com>
2020-09-12 09:48:11 -0700

    Add API endpoint

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 import dataset
+import datetime
 import logging
 
 from bottle import Bottle, route, run, template, request, static_file, response, TEMPLATES
@@ -20,6 +21,30 @@ PROJECT_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)))
 DEFAULT_DB_URI = 'postgresql://primicia:primiciapassword!@0.0.0.0:5432/primicia'
 DB_URI = os.getenv("DATABASE_URL", DEFAULT_DB_URI)
 db = dataset.connect(DB_URI)
+
+ALLOWED_SITES = {
+    "zetatijuana.com",
+    "elvigia.net",
+    "ensenada.net",
+    "el-mexicano.com",
+    "radanoticias.info",
+    "elimparcial.com",
+}
+
+
+# the decorator
+def enable_cors(fn):
+    def _enable_cors(*args, **kwargs):
+        # set CORS headers
+        response.headers['Access-Control-Allow-Origin'] = '*'
+        response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, OPTIONS'
+        response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
+
+        if request.method != 'OPTIONS':
+            # actual request; reply with the actual response
+            return fn(*args, **kwargs)
+
+    return _enable_cors
 
 
 @route("/")
@@ -67,6 +92,62 @@ def visits(id):
 
     response.content_type = 'application/json'
     return dumps({"message": "success"})
+
+
+# API routes
+@route("/api/news")
+@enable_cors
+def api_news():
+    page = request.GET.get("page", 1)
+    order = request.GET.get("order", "relevant")
+    date = request.GET.get("date", None)
+    sites = request.query.getall('sites') or []
+
+    try:
+        page = int(page)
+    except:
+        page = 1
+    limit = 30
+    offset_page = page - 1
+    offset = offset_page * limit
+    start = 1 + (offset_page * limit)
+
+    filters = []
+    order_field = "visits, \"datetimeAdded\"" if order == "relevant" else "\"datetimeAdded\""
+
+    dt_obj = datetime.datetime.strptime(date, '%Y%m%d') if date else datetime.datetime()
+    date_value = dt_obj.strftime("%Y-%m-%d")
+    filters.append(f"\"dateAdded\" = '{date_value}'")
+    
+    if sites:
+        valid_sites = set(sites).intersection(ALLOWED_SITES)
+        if len(valid_sites) > 0:
+            sites_value = ",".join([f"'{site}'" for site in valid_sites])
+            filters.append(f"website IN ({sites_value})")
+
+    filters_query = "WHERE " + " AND ".join(filters) if filters else ""
+
+    # For ordering, the more time has elapsed, the less weight the news is going to have
+    query = """
+    SELECT *
+    FROM news
+    {}
+    ORDER BY {} DESC
+    OFFSET {}
+    LIMIT {}
+    """.format(filters_query, order_field, offset, limit)
+
+    news_result = db.query(query)
+
+    news = []
+    ids = []
+    for new in news_result:
+        new["ellapsed_time"] = ellapsed_time(new["datetimeAdded"])
+        del new["datetimeAdded"]
+        news.append(new)
+
+    response.content_type = 'application/json'
+    return dumps({"data": news})
 
 
 # static files
